0277b302c6991889e26cae95278daf6916282c54
kinziro <kinziro>
2020-06-29 08:48:45 +0000

    fix the filename

--- ex_env/kuka/__init__.py
+++ ex_env/kuka/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='KukaEnvReaching-v0',
-    entry_point='ex_env.kuka.kukaEnvReaching:KukaGymEnv'
+    entry_point='ex_env.kuka.kukaGymEnv:KukaGymEnv'
 )
